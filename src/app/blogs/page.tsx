ecaf6c677c0514c274574959d38b601c465abbf6
jmjubaer <jmjubaer3927@gmail.com>
2025-03-20 22:27:29 +0600

    crate blogs page and show all blogs by server side rendering

---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,29 @@
+import BlogCard from "@/components/ui/BlogCard";
+import { Blog } from "@/types";
+
+const BlogsPage = async () => {
+    const res = await fetch("http://localhost:5000/blogs", {
+        cache: "no-store",
+    });
+    const data = await res.json();
+    return (
+        <div className='w-[90%] mx-auto'>
+            <h1 className='text-3xl text-center my-5 font-bold'>
+                Blogs From <span className='text-teal-600'>NexaBlog</span>
+            </h1>
+            <p className='text-center text-gray-400 w-2/5 mx-auto'>
+                <i>
+                    Dive into the fascinating world of quantum computing, where
+                    unlocking unprecedented computational power.
+                </i>
+            </p>
+            <div className='grid grid-cols-3 gap-7 mt-10'>
+                {data.map((blog: Blog) => (
+                    <BlogCard key={blog?.id} blog={blog} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default BlogsPage;
