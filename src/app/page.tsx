b2cd106efb708f5ebbe680a2a157bfc8316a786d
jmjubaer <jmjubaer3927@gmail.com>
2025-03-20 22:05:07 +0600

    initialize stater project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
-const HomePage = () => {
-  return (
-    <div className="my-10">
-      <h1 className="text-4xl text-center">Latest Blogs</h1>
-    </div>
-  );
+import LatestBlogs from "@/components/LatestBlogs/LatestBlogs";
+
+const HomePage = async () => {
+    const res = await fetch("http://localhost:5000/blogs", {
+        next: {
+            revalidate: 30,
+        },
+    });
+    const data = await res.json();
+    return (
+        <div className='my-10'>
+            <h1 className='text-4xl text-center'>Latest Blogs</h1>
+            <LatestBlogs blogs={data} />
+        </div>
+    );
 };
 
 export default HomePage;
