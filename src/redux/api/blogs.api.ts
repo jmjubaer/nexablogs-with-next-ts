2f53d16ee51c38453071b379b2cf284001922436
jmjubaer <jmjubaer3927@gmail.com>
2025-03-21 23:27:05 +0600

    initalize redux in next app

---
+++ src/redux/api/blogs.api.ts
@@ -0,0 +1,19 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// Define a service using a base URL and expected endpoints
+export const blogApi = createApi({
+    reducerPath: "pokemonApi",
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/api" }),
+    endpoints: (build) => ({
+        getBlogs: build.query({
+            query: () => ({
+                url: "/blogs",
+                method: "GET",
+            }),
+        }),
+    }),
+});
+
+// Export hooks for usage in functional components, which are
+// auto-generated based on the defined endpoints
+export const { useGetBlogsQuery } = blogApi;
